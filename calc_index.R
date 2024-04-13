320b46d9a50dc903289e0f776d6e4859f86f4560
Victor Diaz <66495032+viedro@users.noreply.github.com>
2024-04-13 18:29:43 -0500

    Update calc_index.R

--- calc_index.R
+++ calc_index.R
@@ -2,7 +2,7 @@
 ## TÍTULO : Cálculo de índices extremos climáticos mediante archivos netcdf
 ## AUTOR : Victor Eduardo Diaz Romero
 ## CORREO : 20180177@lamolina.edu.pe
-## Código también disponible en: https://github.com/viedro/Extreme_index_calcu/blob/main/C_index.R
+## Código también disponible en: https://github.com/viedro/Extreme_index_calcu/edit/main/calc_index.R
 #################################################################
 
 
@@ -14,7 +14,7 @@
 library(raster)
 
 #Directorio de trabajo
-setwd("C:/Users/ASUS/Desktop/prof_Ibañez")
+setwd("C:/Users/ASUS/Desktop/Extreme_index")
 
 #Paquete para el manejo de datos netcdf
 #install.packages("ncdf4")
@@ -30,7 +30,7 @@ str(pr) ; class(pr)     ####Dimensiones y tipo de objeto
 lat <- ncvar_get(pp,"lat")
 lon <- ncvar_get(pp,"lon")
 lat <- rev(lat)
-pr <- pr[,ncol(pr):1 ,]
+pr <- pr[  ,ncol(pr):1,]
 
 #Extracción de tiempo
 time <- ncvar_get(pp,"time")
@@ -43,21 +43,22 @@ nc_close(pp)
 time2 <- as.Date("1981-01-01")+time
 time2
 
+class(time2)
 t_max <- max(as.numeric(format(time2,"%Y")))
 t_min <- min(as.numeric(format(time2,"%Y")))
 
 ######## Recortando rectangulo de ubicación de cuencas
 ######## En este caso las cuencas a estudiar se encuetran entre  -85°W -77°W, -8.5°S -2.5°S
 ######## Esto se hace para no tomar toda la región disponible y acelerar el proceso de calculo
-max_lon <- -77
-min_lon <- -85
-max_lat <- -2.5
-min_lat <- -8.5
+max_lon <- -74
+min_lon <- -78
+max_lat <- -9
+min_lat <- -14
 
 ########Correccion final de arrays
 pr <- pr[which(lon<= max_lon & lon>= min_lon ),
          which(lat<=max_lat & lat>=min_lat),]
-
+str(pr)
 ########Corrección final de dimensiones
 lon <- lon[which(lon<= max_lon & lon>= min_lon )]
 lat <- lat[which(lat<= max_lat & lat>= min_lat)]
@@ -66,8 +67,7 @@ lat <- lat[which(lat<= max_lat & lat>= min_lat)]
 # Por defecto la creación de un netcdf con todos sus metadatos es muy engorrosa, este paquete lo hace en pocas lineas
 #install.packages("easyNCDF")
 library(easyNCDF)
-#install.packages("oceanmap")
-library(oceanmap)
+
 
 #Crearemos nuevos archivos netcdf con el area recortada y con metadatos modificados para que el paquete climdex.pcic.ncdf que instalaremos mas adelante
 #pueda funcionar correctamente
@@ -94,7 +94,7 @@ ArrayToNc(list(pr, lon, lat,time), paste0("pr_NAM44_CanRCM4_ERAINT_r1i1p1_",t_mi
 
 ##############
 
-#install.packages("devtools")
+#install.packages("devtools")   ####nstalar paquetes desde github
 library(devtools)
 
 #instalación de climdex.pcic desde github
@@ -112,11 +112,11 @@ author.data <- list(institution="Looney Bin", institution_id="LBC")
 
 #Aqui seleccionar 2 nucleos menos que la cantidad de nucleos de tu pc, para un trabajo en paralelo
 #Por ejemplo yo tengo 8 nucleos, le dare el trabajo a 5 o 6 nucleos
-#como seguramente las pcs del lab sean de 4 nucleos, darle el trabajo a 2 nucleos
+
 n_nucleos <- 6
 
 #Creando directorio donde guardar los resultados
-name_dir <- "hist_Accessssssss"
+name_dir <- "hist_MPI"
 dir.create(name_dir)
 
 #Con esto calcularemos todos los indices
@@ -124,64 +124,16 @@ create.indices.from.files(input.files,paste0(name_dir,"/"),input.files[1], autho
                           base.range=c(t_min, t_max), parallel= n_nucleos,climdex.time.resolution ="annual")
 
 
+library(rasterVis)
+library(paletteer)
 ####################################################################################
 ############# Parte 2 calculo de acumulados, generacion de archivos raster y graficado
-#install.packages("rgdal")
-library(rgdal)
-#install.packages("sp")
-library(sp)
-#install.packages("sf")
-library(sf)
-#install.packages("maptools")
-library(maptools)
-
-#Shapefile de cuencas
-shp2 <- readOGR(dsn = "norte_cuencas.shp")
-
-#Modificando proyeccion del shapefile
-crs_destino <- CRS("+proj=longlat +datum=WGS84")
-
-# Realizar la transformación de coordenadas
-shp <- spTransform(shp2, crs_destino)
-
-# Extrayendo departamneteos y provincias
-mapaSHP <- readShapeLines('DEPARTAMENTOS_inei_geogpsperu_suyopomalia.shp')
-mapaSHP <- mapaSHP[mapaSHP$NOMBDEP == "PIURA" | mapaSHP$NOMBDEP == "TUMBES"|mapaSHP$NOMBDEP == "LAMBAYEQUE"|mapaSHP$NOMBDEP == "CAJAMARCA"|mapaSHP$NOMBDEP == "AMAZONAS",]
-
-provinSHP <- readShapeLines("PROVINCIAS_inei_geogpsperu_suyopomalia.shp")
-provinSHP <- provinSHP[provinSHP$NOMBDEP %in% c("PIURA", "TUMBES", "LAMBAYEQUE", "CAJAMARCA", "AMAZONAS"), ]
-
-landuse <- readShapePoly("PROVINCIAS_inei_geogpsperu_suyopomalia.shp") 
-landuse<- landuse[landuse$NOMBDEP %in% c("PIURA", "TUMBES", "LAMBAYEQUE", "CAJAMARCA", "AMAZONAS"), ]
-
-#install.packages("rgeos")
-library(rgeos)
-
-#Obtencion de centroides
-centr <- gCentroid(landuse, byid = TRUE)
-centr <- SpatialPointsDataFrame(centr, data= landuse@data)
-
-#install.packages("rasterVis")
-library(rasterVis)
-#install.packages("paleteer")
-library(paletteer)
-#install.packages("rnaturalearth")
-library(rnaturalearth)
-#install.packages("dplyr")
-library(dplyr)
-#install.packages("grid")
-library(grid)
-library(latticeExtra)
-#install.packages("lattice")
-library(lattice)
-
 
 # Obtener una lista de todos los archivos en el directorio
 archivos <- list.files(name_dir)
 
 # Filtrar los archivos que contienen la palabra "ETCCDI"
 archivos_con_ETCCDI <- archivos[grep("ETCCDI", archivos)]
-archivos_con_ETCCDI <-  archivos_con_ETCCDI[c(1,6:31)]
 
 # Años a trabajar:
 year <- t_min:t_max
@@ -192,51 +144,82 @@ year
 # En este caso seleccionare todo el periodo
 period <- which(year>=t_min & year<=t_max)
 
+shp <- shapefile('DEPARTAMENTOS_inei_geogpsperu_suyopomalia.shp')
+shp <- shp[shp$NOMBDEP == "LIMA",]
 
+library(sf)
+shape <- st_read("DEPARTAMENTOS_inei_geogpsperu_suyopomalia.shp")
+shape <- shape[shape$NOMBDEP == "LIMA", ]
+
+shape2 <- st_read("PROVINCIAS_inei_geogpsperu_suyopomalia.shp")
+shape2 <- shape2[shape2$NOMBDEP == "LIMA", ]
+
+#Filtro de nombre de indices
+indices <- sub("ETCCDI.*", "", archivos_con_ETCCDI)
+
+#install.packages("ggplot2")
+library(ggplot2)
+library(metR)
+library(ggspatial)
 ##########Bucle 
-
-funciones <- c("altcdd","csdi","cwd","fd","gsl","id","prcptot","r10mm","r1mm","r20mm","r95p","r99p","rx1day","rx5day","sdii","su","tn10p","tn90p","tr","tx10p","tx90p","wsdi")
-
 ##Bucle para lectura de netcdf como raster,el interpolado, recorte , graficado y generqacion de raster de cada indice.
-for (i in 1:length(funciones)){
-  
+for (i in 1:length(archivos_con_ETCCDI)){
   tryCatch({
-    archivos2 <- archivos_con_ETCCDI[-4]
-    data <- archivos2[grep(funciones[i], archivos2)]
+    data <- archivos_con_ETCCDI[i]
     
     data2 <- brick(paste0(name_dir,"/",data))
-    
+   
     rs <- calc(data2[[c(period)]],sum)
     
-    u_f <- disaggregate(rs,fact= 10,method="bilinear")
+    rs <- disaggregate(rs,fact= 10,method="bilinear")
     
-    u_f2<- raster::crop(u_f,shp) %>%
+    u_f2<- raster::crop(rs,shp) %>%
       raster::mask(shp)
     
   },error=function(e){})
   
-  png(paste0(name_dir,"/",funciones[i], "_ACCES1_hist.png"), width = 12, height = 11, res = 700, units = "in")
+  
+  u_f3 <- as.data.frame(u_f2,xy=T)
+  u_f3 <- na.omit(u_f3)
+  
+
+
   tryCatch({
-    image <- levelplot(u_f2, margin = FALSE, xlab = "Longitud", cuts = 100, ylab = "Latitud", 
-                       col.regions = rev(paletteer_c("grDevices::Spectral", 101)), maxpixels = 1e20, 
-                       main = paste0("Índice ", toupper(funciones[i]), " Histórico Modelo Acces1-0 \n Costa, Sierra Norte 1981-2005")) +
-      layer(sp.lines(mapaSHP, lwd = 1.8, col = 'black')) +
-      layer(sp.lines(provinSHP, lwd = 1, col = 'gray30')) +
-      layer(sp.points(centr, pch = 20, cex = 1, col = "black")) +
-      layer(sp.text(coordinates(centr) + 0.07, txt = landuse$NOMBPROV, col = "black", cex = 0.6))
+    ggplot(data=shape)+
+      geom_contour_fill(data= u_f3,aes(x=x,y=y,z=layer),bins=30)+
+      geom_sf(data=shape2,size=1,fill="transparent",col="black")+
+      geom_sf(fill="transparent",size=1,col="black") +
+      theme(plot.title=element_text(hjust=0.5),legend.key.height = unit(2,"cm"))+ theme(panel.grid.major = element_line(color = gray(.5), linetype = "dashed", size = 0.5))+
+      scale_fill_gradientn(colours=rev(paletteer_c("grDevices::Spectral",30)))+theme(plot.title = element_text(hjust = 0.5,face = "bold"))+
+      theme_bw()+
+      xlab("Longitud")+
+      ylab("Latitud")+
+      theme(
+        panel.grid.major = element_line(color="gray60", linetype="dashed", size=0.25),legend.key.height = unit(1.5,"cm"),
+        panel.ontop = TRUE,
+        panel.background = element_rect(color=NA, fill=NA),  # Change the background to white
+        legend.key = element_rect(color="black"),  # Make the text bold
+        plot.title = element_text(hjust=0.5, face="bold"),
+        strip.background = element_rect(fill="white",color=NA),
+        strip.text = element_text(color="black", face="bold",size=15)
+      ) +
+      annotation_scale(location = "bl", width_hint = 0.4) +
+      annotation_north_arrow(location = "tr", which_north = "true", 
+                             pad_x = unit(0.0, "in"), pad_y = unit(0.2, "in"),
+                             style = north_arrow_fancy_orienteering)+ggtitle(paste0("Índice ", toupper(indices[i]), " Histórico Modelo MPI \n Lima 1981-2005"))+ labs(fill="")
+    
+    ggsave(paste0(name_dir,"/",indices[i], "_MPI_hist.png"), width = 7, height = 8,units = 'in',dpi=700)
+
   },error=function(e){})
   
-  tryCatch({
-    print(image)
-  },error=function(e){})
-  dev.off() # Cerrar el dispositivo gráfico
+
   
   
   tryCatch({
-    writeRaster(u_f2, filename = paste0(name_dir,"/",funciones[i], "_ACCES1_hist.tif"), format = "GTiff", overwrite = TRUE)
+    writeRaster(u_f2, filename = paste0(name_dir,"/",indices[i], "_MP1_hist.tif"), format = "GTiff", overwrite = TRUE)
   },error=function(e){})
   
-  if (i == length(funciones)) {
+  if (i == length(archivos_con_ETCCDI)) {
     cat("Índices calculados y exportados , revisar directorio creado\n")
     cat("   _______________\n")
     cat(" /                          \\\n")
@@ -250,7 +233,6 @@ for (i in 1:length(funciones)){
     cat("        / \\\n")
   }
 }
-
 
 
 
